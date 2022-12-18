8160cac1733689f46024a855dab3c27c971ddf8f
kirimaki <kirimaki@nate.com>
2022-12-18 20:29:29 +0900

    '쇼핑몰'

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="black-bg" v-if="isModalOpen == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="ModalOpen(false);">닫기</button>
+    </div>
+  </div>
+  
+  <div class="menu">
+    <a v-for="(menuName,i) in menu" :key='i'>{{menuName}}</a>
+  </div>
+
+  <div v-for="(product,i) in products" :key='i'>
+    <h4>{{product.title}}</h4>
+    <!--<img class="room-img" src="./assets/"+{{image[i]}}+".png">-->
+    <img @click="ModalOpen(true);" class="room-img" :src=product.image>
+    <p>{{product.price}}원</p>
+    <p>좋아요 수 : {{heartCount[i]}}</p>
+    <button @click="heartAdd(i);">좋아요</button>
+  </div>
+  <button v-on:click="heartShot();">하트뿅뿅</button>
+  <span id="heart">{{heartSpan}}</span>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import productData from './assets/product.js';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      menu : ['Home', 'Products', 'About', 'Help'],
+      products : productData,
+      heartCount : [0, 0, 0],
+      heartSpan : "",
+      isModalOpen : false,
+    }
+  },
+  methods : {
+    heartAdd(index) {
+      this.heartCount[index] += 1;
+    },
+    heartShot() {
+      this.heartSpan += "♥";
+    },
+    ModalOpen(toggle) {
+      this.isModalOpen = toggle;
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +61,42 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding:15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color : white;
+  padding: 10px;
+}
+
+#heart {
+  color : red;
+}
+
+.room-image {
+  width:100%;
+  margin-top : 40px;
+}
+
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgb(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
---
+++ src/assets/product.js
@@ -0,0 +1,42 @@
+export default [{
+    id : 0,
+    title: "Brown Check",
+    image: "./갈색체크치마.png",
+    content: "18년 신축공사한 남향 원룸 ☀️, 공기청정기 제공",
+    price: 30000
+    },
+    {
+    id : 1,
+    title: "Star Jean(boot Fit)",
+    image: "/img/부츠컷스타청바지.png",
+    content: "침실만 따로 있는 공용 셰어하우스입니다. 최대 2인 가능",
+    price: 50000
+    },
+    {
+    id : 2,
+    title: "Stripe Star neat(Standard)",
+    image: "./assets/스트라이프스타니트.png",
+    content: "금산오거리 역세권 아파트입니다. 애완동물 불가능 ?",
+    price: 38000
+    },
+    {
+    id : 3,
+    title: "colorful Star neat",
+    image: "./assets/알록달록스타니트.png",
+    content: "무암동인근 2인용 원룸입니다. 전세 전환가능",
+    price: 40000
+    },
+    {
+    id : 4,
+    title: "Star Jean(Wide Fit)",
+    image: "./assets/통별청바지.png",
+    content: "탄천동 아파트 월세, 남향, 역 5분거리, 허위매물아님",
+    price: 44000
+    },
+    {
+    id : 5,
+    title: "Star Jean2(boot Fit)",
+    image: "./assets/부츠컷스타청바지2.png",
+    content: "반지하 원룸입니다. 비올 때 물가끔 새는거 빼면 좋아요",
+    price: 37000
+  }];
